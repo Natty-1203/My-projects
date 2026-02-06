e35c5457132836a4f3adbc1420189fd21e61ddee
Natty-1203 <natatanatata79@gmail.com>
2026-02-06 15:06:10 +0300

    fix running

--- reunite-hub/src/Routes.jsx
+++ reunite-hub/src/Routes.jsx
@@ -1,19 +1,19 @@
 import { createBrowserRouter } from "react-router-dom";
 import Layout from "./Componenets/Layout.jsx";
-import Home from "./Pages/Home.jsx";
+import Home from "./pages/Home.jsx";
 import SubmitForm from "./pages/SubmitForm.jsx";
-import Volunteer from "./Pages/Volunteer.jsx";
-import Resources from "./Pages/Resources.jsx"; // Import the Resources component
-import Error_page from "./pages/Error_page.jsx"; // Import the ErrorPage component
-import AlertsMaps from "./Pages/AlertsMaps.jsx"; // Import the AlertsMaps component
-import Login from "./Pages/Login.jsx"; // Import the Login component
-import ForgotPassword from "./Pages/ForgotPassword.jsx"; // Import the ForgotPassword component
-import SignUp from "./Pages/SignUp.jsx"; // Import the SignUp component
+import Volunteer from "./pages/Volunteer.jsx";
+import Resources from "./pages/Resources.jsx"; 
+import Error_page from "./pages/Error_page.jsx"; 
+import AlertsMaps from "./Pages/AlertsMaps.jsx"; 
+import Login from "./pages/Login.jsx"; 
+import ForgotPassword from "./pages/ForgotPassword.jsx"; 
+import SignUp from "./pages/SignUp.jsx";
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Layout />, // This layout will apply to all child routes
-    errorElement: <Error_page />, // Render Error_page for invalid routes or errors
+    element: <Layout />, 
+    errorElement: <Error_page />, 
     children: [
       {
         path: "/",
